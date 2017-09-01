ab341cbfe9bb6f7de7453b623168c890781c2695
oxygenyoyo <oxygen.uo@gmail.com>
2017-09-01 20:59:05 +0700

    redirect login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin/question';
 
     /**
      * Create a new controller instance.
